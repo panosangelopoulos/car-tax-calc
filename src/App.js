af8a15a91ee4c693984da50eb83eed0f9abd5bad
panosangelopoulos <panos.angelopoulos@outlook.com>
2020-07-12 00:31:40 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,179 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import {
+  Header,
+  HeaderName,
+  DatePicker,
+  DatePickerInput,
+  NumberInput,
+} from 'carbon-components-react'
 
-function App() {
+import { LogoGithub16 } from '@carbon/icons-react'
+
+import { checkCarTaxType, checkCO2Tax } from './utils'
+
+import 'carbon-components/css/carbon-components.min.css'
+
+const ContentArea = styled.div`
+  height: 100%;
+  width: 100%;
+  text-align: center;
+
+  margin-top: 4em;
+
+  .bx--date-picker {
+    width: 100%;
+  }
+
+  .bx--date-picker-container {
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .bx--date-picker,
+  .bx--date-picker--single,
+  .bx--date-picker__input {
+    width: 100% !important;
+  }
+
+  .bx--form-item {
+    width: 100%;
+    min-width: 328px;
+  }
+
+  .bx--number {
+    min-width: 328px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`
+
+const FieldWrapper = styled.div`
+  margin: 20px;
+`
+
+const FooterText = styled.div`
+  margin: 0 2em;
+
+  display: flex;
+  align-items: center;
+`
+
+const App = () => {
+  const [date, setDate] = React.useState()
+  const [cc, setCC] = React.useState(null)
+  const [co2, setCO2] = React.useState(null)
+
+  const isCC = date < new Date('2010-11-01')
+  const isCo2 = date >= new Date('2010-11-01')
+
+  const taxesFromCC = isCC && checkCarTaxType(date, cc)
+  const taxesFromCO2 = isCo2 && checkCO2Tax(co2)
+
+  if (cc > 10000) {
+    alert('Ο υπολογισμός για τα συγκεκριμένα κυβικά είναι αδύνατος!')
+    window.location.href = 'https://www.youtube.com/watch?v=vmXw4cJsESQ'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div
+      className="App"
+      style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}
+    >
+      <Header aria-label="Τέλη κυκλοφορίας 2020">
+        <HeaderName href="#" prefix="2020">
+          Τέλη κυκλοφορίας
+        </HeaderName>
+      </Header>
+      <ContentArea>
+        <h5>Υπολογισμός των τελών κυκλοφορίας (2020)</h5>
+        <FieldWrapper>
+          <DatePicker
+            datePickerType="single"
+            dateFormat="m/Y"
+            value={date}
+            onChange={(eventOrDates) => {
+              const value = eventOrDates.target
+                ? eventOrDates.target.value
+                : eventOrDates[0]
+              setDate(value)
+            }}
+          >
+            <DatePickerInput
+              id="date-picker-input-id"
+              labelText={
+                'Επιλέξτε την ημερομηνία ταξινόμησης του οχήματός σας.'
+              }
+              locale="gr"
+              placeholder={`πχ. 07/2012`}
+            />
+          </DatePicker>
+        </FieldWrapper>
+
+        {isCC && (
+          <>
+            <FieldWrapper>
+              <NumberInput
+                id="cc-input"
+                helperText="Αναφέρεται τον κυβισμό του οχήματός σας."
+                invalidText="Ο αριθμός των κυβικών σας είναι σωστός! Επιλέξτε έναν αριθμό από 0 έως 10000."
+                label="Επιλέξτε τον κινητήρα του αυτοκινήτου (σε cc.)."
+                max={10000}
+                min={0}
+                step={1}
+                value={cc}
+                onChange={(e) => setCC(e.target.value)}
+                placeholder={1200}
+              />
+            </FieldWrapper>
+
+            {taxesFromCC && (
+              <FieldWrapper>
+                Το συνολικό κόστος των τελών σας είναι
+                <strong> {taxesFromCC}€</strong>
+              </FieldWrapper>
+            )}
+          </>
+        )}
+
+        {isCo2 && (
+          <>
+            <FieldWrapper>
+              <NumberInput
+                id="co2-input"
+                invalidText="Ο αριθμός των ρύπων δεν ειναι σωστός! Επιλέξτε έναν αριθμό από 0 έως 250."
+                label="Επιλέξτε την ποσότητα εκπομπής ρύπων (CO2)."
+                max={1000}
+                min={0}
+                step={1}
+                value={co2}
+                onChange={(e) => setCO2(e.target.value)}
+                placeholder={134}
+              />
+            </FieldWrapper>
+
+            {taxesFromCO2 && (
+              <FieldWrapper>
+                Το συνολικό κόστος των τελών σας είναι
+                <strong> {taxesFromCO2}€</strong>
+              </FieldWrapper>
+            )}
+          </>
+        )}
+      </ContentArea>
+
+      <FooterText>
+        <LogoGithub16 />:
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/panosangelopoulos/car-tax-calc"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          https://github.com/panosangelopoulos/car-tax-calc
         </a>
-      </header>
+      </FooterText>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/utils.js
@@ -0,0 +1,104 @@
+export const checkCarTaxType = (date, cc) => {
+  const selectedDateFromInput = new Date(date)
+
+  if (selectedDateFromInput <= new Date('2000-12-31')) {
+    if (cc <= 300) {
+      return 22
+    } else if (cc >= 301 && cc <= 785) {
+      return 55
+    } else if (cc >= 786 && cc <= 1071) {
+      return 120
+    } else if (cc >= 1072 && cc <= 1357) {
+      return 135
+    } else if (cc >= 1358 && cc <= 1548) {
+      return 225
+    } else if (cc >= 1549 && cc <= 1738) {
+      return 250
+    } else if (cc >= 1739 && cc <= 1928) {
+      return 280
+    } else if (cc >= 1929 && cc <= 2357) {
+      return 615
+    } else if (cc >= 2358 && cc <= 3000) {
+      return 820
+    } else if (cc >= 3001 && cc <= 4000) {
+      return 1025
+    } else {
+      return 1230
+    }
+  } else if (
+    selectedDateFromInput > new Date('2001-01-01') &&
+    selectedDateFromInput <= new Date('2005-12-31')
+  ) {
+    if (cc >= 0 && cc <= 300) {
+      return 22
+    } else if (cc >= 301 && cc <= 785) {
+      return 55
+    } else if (cc >= 786 && cc <= 1071) {
+      return 120
+    } else if (cc >= 1072 && cc <= 1357) {
+      return 135
+    } else if (cc >= 1358 && cc <= 1548) {
+      return 240
+    } else if (cc >= 1549 && cc <= 1738) {
+      return 265
+    } else if (cc >= 1739 && cc <= 1928) {
+      return 300
+    } else if (cc >= 1929 && cc <= 2357) {
+      return 630
+    } else if (cc >= 2358 && cc <= 3000) {
+      return 840
+    } else if (cc >= 3001 && cc <= 4000) {
+      return 1055
+    } else {
+      return 1260
+    }
+  } else if (
+    selectedDateFromInput > new Date('2006-01-01') &&
+    selectedDateFromInput <= new Date('2010-10-31')
+  ) {
+    if (cc >= 0 && cc <= 300) {
+      return 22
+    } else if (cc >= 301 && cc <= 785) {
+      return 55
+    } else if (cc >= 786 && cc <= 1071) {
+      return 120
+    } else if (cc >= 1072 && cc <= 1357) {
+      return 135
+    } else if (cc >= 1358 && cc <= 1548) {
+      return 255
+    } else if (cc >= 1549 && cc <= 1738) {
+      return 280
+    } else if (cc >= 1739 && cc <= 1928) {
+      return 320
+    } else if (cc >= 1929 && cc <= 2357) {
+      return 690
+    } else if (cc >= 2358 && cc <= 3000) {
+      return 920
+    } else if (cc >= 3001 && cc <= 4000) {
+      return 1150
+    } else {
+      return 1380
+    }
+  } else if (selectedDateFromInput > new Date('2010-11-01'))
+    return 'No Calculation'
+}
+
+export const checkCO2Tax = co2 => {
+  if (co2 <= 90) {
+    return 0
+  } else if (co2 >= 91 && co2 <= 120) {
+    return co2 * 0.9
+  } else if (co2 >= 121 && co2 <= 140) {
+    return co2 * 0.98
+  } else if (co2 >= 141 && co2 <= 160) {
+    return co2 * 1.2
+  } else if (co2 >= 161 && co2 <= 180) {
+    return co2 * 1.85
+  } else if (co2 >= 181 && co2 <= 200) {
+    return co2 * 2.45
+  } else if (co2 >= 201 && co2 <= 250) {
+    return co2 * 3.05
+  } else if (co2 >= 251) {
+    return co2 * 3.72
+  }
+}
